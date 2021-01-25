8323e1e50d0403bea35afa0962febe29caeb20ab
breckbaldwin <git@lingpipe.com>
2021-01-25 12:13:22 -0500

    catchup

--- run.R
+++ run.R
@@ -5,16 +5,12 @@ library("cmdstanr")
 # >Session>Set Working Directory>To Source File Location
 source("data/SIR_data.R")
 model <- cmdstan_model("stan/Simple_SIR.stan")
-stan_data <- list(N_t=N_t, stoi=stoi, t=t, y0=y0, B=B)
+stan_data <- list(n_days=N_t, stoi=stoi, t=t, y0=y0, B=B)
 fit <- model$sample(data=stan_data, output_dir="output")
 print(fit)
 
 
 
-system(paste("stan/Simple_SIR sample data",
-            "file=data/SIR_data.R > output/out1"))
-
-cat(paste(readLines('output/out1'), "\n", sep=""), sep="")
 
 
 
